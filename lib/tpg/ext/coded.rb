696e1c8a7c22e5239d9f89f898f946ddbd2abd1d
Adam B. Goldstein <abgoldstein@gmail.com>
2012-08-15 14:36:17 -0400

    Removing pry for debugging with patient builder.

--- lib/tpg/ext/coded.rb
+++ lib/tpg/ext/coded.rb
@@ -8,7 +8,6 @@ module HQMF
     # @return A Hash of code sets corresponding to the given oid, each containing one randomly selected code.
     def self.select_codes(oid, value_sets)
       # Pick the value set for this DataCriteria. If it can't be found, it is an error from the value set source. We'll add the entry without codes for now.
-      binding.pry
       index = value_sets.index{|value_set| value_set["oid"] == oid}
       value_sets = index.nil? ? { "code_sets" => [] } : value_sets[index]
       
