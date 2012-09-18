de63bc484b759310b1efe70aa20e7c0bd05bd6d1
Adam B. Goldstein <abgoldstein@gmail.com>
2012-09-18 16:13:38 -0400

    Introducing the measure_ids field to Records so we can note the intended target measures for a patient when applicable.

--- lib/tpg/ext/record.rb
+++ lib/tpg/ext/record.rb
@@ -1,4 +1,5 @@
 # Extensions to the Record model in health-data-standards to add needed functionality for test patient generation
 class Record
+  field :measure_ids, type: Array
   field :source_data_criteria, type: Array
 end
