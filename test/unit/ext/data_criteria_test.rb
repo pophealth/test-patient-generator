158d26cb4fde42cdf17b0f7040ac75416b5c23c5
Adam Goldstein <agoldstein@mitre.org>
2012-12-17 09:42:26 -0500

    Adding tests for patient characteristics.

--- test/unit/ext/data_criteria_test.rb
+++ test/unit/ext/data_criteria_test.rb
@@ -2,23 +2,31 @@ require 'test_helper'
 require 'hqmf2js'
 
 class DataCriteriaTest < MiniTest::Unit::TestCase
-  def setup
-    # Generate the patients and export them in the requested format to the out_path
-    # patients = HQMF::Generator.generate_qrda_patients(
-    #   {hqmf.id => hqmf.referenced_data_criteria},
-    #   {hqmf.id => value_sets})
-    # @patient = patients[hqmf.id]
+  def test_modify_patient
+
   end
-  
-  def test_has_all_expected_entries
-    # assert @patient.medications.length > 0
-    # assert @patient.procedures.length > 0
-    # assert @patient.encounters.length > 0
+
+  def test_modify_patient_with_characteristic
+
   end
-  
-  def test_entry_descriptions_include_appropriate_oid
-    # assert @patient.medications[0].description.include?('2.16.840.1.113883.3.464.0001.430')
-    # assert @patient.encounters[0].description.include?('2.16.840.1.113883.3.464.0001.49')
-    # assert @patient.procedures[0].description.include?('2.16.840.1.113883.3.464.0001.143')
+
+  def test_derive_entry
+
+  end
+
+  def test_modify_entry_with_values
+
+  end
+
+  def test_modify_entry_with_negation
+
+  end
+
+  def test_modify_entry_with_fields
+
+  end
+
+  def test_modify_patient_with_entry
+    
   end
 end
