9d0739625df12b7ea66719da496371e64bd26a84
Andre Quina <andre.quina@gmail.com>
2013-02-06 15:08:04 -0500

    fixed code description overlay for updated HDS

--- lib/tpg/generation/exporter.rb
+++ lib/tpg/generation/exporter.rb
@@ -5,9 +5,9 @@ module TPG
     # @param [Array] patients All of the patients that will be exported.
     # @param [String] format The desired format for the patients to be in.
     # @return A zip file containing all given patients in the requested format.
-    def self.zip(patients, format, concept_map=nil)
+    def self.zip(patients, format)
       file = Tempfile.new("patients-#{Time.now.to_i}")
-      
+      html_exporter = HealthDataStandards::Export::HTML.new
       Zip::ZipOutputStream.open(file.path) do |z|
         xslt = Nokogiri::XSLT(File.read("public/cda.xsl"))
         patients.each do |patient|
@@ -24,7 +24,7 @@ module TPG
             z << HealthDataStandards::Export::CCDA.new.export(patient)
           elsif format == "html"
             z.put_next_entry("#{next_entry_path}.html")
-            z << html_contents(patient, concept_map)
+            z << html_contents(patient, html_exporter)
           elsif format == "json"
             z.put_next_entry("#{next_entry_path}.json")
             z << JSON.pretty_generate(JSON.parse(patient.to_json))
@@ -43,13 +43,13 @@ module TPG
     # @return A zip file containing all of the QRDA Category 1 patients that were passed in.
     def self.zip_qrda_html_patients(measure_patients)
       file = Tempfile.new("patients-#{Time.now.to_i}")
-      
+      html_exporter = HealthDataStandards::Export::HTML.new
       Zip::ZipOutputStream.open(file.path) do |zip|
         xslt = Nokogiri::XSLT(File.read("public/cda.xsl"))
         measure_patients.each do |measure, patient|
           # Create a directory for this measure and insert the HTML for this patient.
           zip.put_next_entry(File.join(measure, "#{patient_filename(patient)}.html"))
-          zip << html_contents(patient)
+          zip << html_contents(patient, html_exporter)
         end
       end
       
@@ -82,8 +82,8 @@ module TPG
     #
     # @param [Record] patient The Record for which we're generating HTML content.
     # @return HTML content to be exported for a Record.
-    def self.html_contents(patient, concept_map=nil)
-      HealthDataStandards::Export::HTML.new.export(patient, concept_map)
+    def self.html_contents(patient, exporter = HealthDataStandards::Export::HTML.new)
+      exporter.export(patient)
     end
     
     # Join the first and last name with an underscore and replace any other punctuation that might interfere with file names.
